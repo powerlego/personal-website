fc883f9fa5b45bbd591afb7b0e5a4108ab940c47
powerlego <11838540+powerlego@users.noreply.github.com>
2023-06-17 10:52:03 -0400

    Added temp page for about me

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutMe() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">About Me</h1>
+      <p className="text-2xl">I&apos;m a software engineer who loves to build things.</p>
+    </div>
+  );
+}
