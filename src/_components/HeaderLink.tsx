a3d59b57452d8b49bbe4cdd3fea8d9664af66e64
powerlego <11838540+powerlego@users.noreply.github.com>
2023-06-17 10:50:42 -0400

    Added custom Link to change color depending on page loaded

---
+++ src/_components/HeaderLink.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type HeaderLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function HeaderLink({ href, children, className }: HeaderLinkProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  return (
+    <Link
+      href={href}
+      className={`${isActive ? "text-blue-700 dark:text-blue-500" : "text-black dark:text-white"} font-bold text-lg ${
+        className ?? ""
+      }`}
+    >
+      {children}
+    </Link>
+  );
+}
