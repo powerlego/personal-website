2792ceae15f8bcf986c838b69a3c34bf12db0258
powerlego <11838540+powerlego@users.noreply.github.com>
2023-06-17 10:51:49 -0400

    removed main html component since it is not necessary here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </div>
   );
 }
