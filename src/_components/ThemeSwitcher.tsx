a3bac63555e997aae80f3e64cf837b079e18d345
powerlego <11838540+powerlego@users.noreply.github.com>
2023-06-18 06:57:27 -0400

    Added component to allow for switching between color themes

--- src/_components/ThemeSwitcher.tsx
+++ src/_components/ThemeSwitcher.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React from "react";
+import { FaSun, FaMoon, FaDesktop } from "react-icons/fa";
 
 type ThemeSwitcherProps = {
   className?: string;
 };
 
 export default function ThemeSwitcher({ className }: ThemeSwitcherProps) {
-  const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = React.useState("dark");
   const [themeSelector, setThemeSelector] = React.useState("system");
 
   const systemThemeListener = (event: MediaQueryListEvent) => {
@@ -23,39 +24,74 @@ export default function ThemeSwitcher({ className }: ThemeSwitcherProps) {
   }, [theme]);
 
   React.useEffect(() => {
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
     if (themeSelector === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
       systemTheme.addEventListener("change", systemThemeListener);
-      return () => systemTheme.removeEventListener("change", systemThemeListener);
+      systemTheme.matches ? setTheme("dark") : setTheme("light");
     }
+    return () => systemTheme.removeEventListener("change", systemThemeListener);
   }, [themeSelector]);
 
   return (
     <div
-      className="w-fit p-1 flex rounded-full border border-solid border-gray-900 dark:border-gray-100"
       role="radiogroup"
+      className={`w-fit p-1 flex rounded-full border border-solid border-gray-900 dark:border-gray-100 ${
+        className ?? ""
+      }`}
     >
       <button
         aria-checked={themeSelector === "light"}
         aria-label="Switch to light theme"
-        className="w-8 h-8 rounded-full flex items-center justify-center border-0 fill-gray-600 dark:fill-gray-400 aria-checked:fill-black dark:aria-checked:fill-white"
+        className="w-8 h-8 rounded-full flex items-center justify-center border-0 group  aria-checked:bg-gray-400 dark:aria-checked:bg-gray-600"
         role="radio"
         type="button"
-      />
+        onClick={() => {
+          setThemeSelector("light");
+          setTheme("light");
+        }}
+      >
+        <FaSun
+          className="fill-gray-600 dark:fill-gray-400 group-aria-checked:fill-black dark:group-aria-checked:fill-white align-middle"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+        />
+      </button>
       <button
         aria-checked={themeSelector === "system"}
         aria-label="Switch to system theme"
-        className="w-8 h-8 rounded-full flex items-center justify-center border-0 fill-gray-600 dark:fill-gray-400 aria-checked:fill-black dark:aria-checked:fill-white"
+        className="w-8 h-8 rounded-full flex items-center justify-center border-0 group aria-checked:bg-gray-400 dark:aria-checked:bg-gray-700"
         role="radio"
         type="button"
-      />
+        onClick={() => {
+          setThemeSelector("system");
+        }}
+      >
+        <FaDesktop
+          className="fill-gray-600 dark:fill-gray-400 group-aria-checked:fill-black dark:group-aria-checked:fill-white align-middle"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+        />
+      </button>
       <button
         aria-checked={themeSelector === "dark"}
         aria-label="Switch to dark theme"
-        className="w-8 h-8 rounded-full flex items-center justify-center border-0 fill-gray-600 dark:fill-gray-400 aria-checked:fill-black dark:aria-checked:fill-white"
+        className="w-8 h-8 rounded-full flex items-center justify-center border-0 group aria-checked:bg-gray-400 dark:aria-checked:bg-gray-600"
         role="radio"
         type="button"
-      />
+        onClick={() => {
+          setThemeSelector("dark");
+          setTheme("dark");
+        }}
+      >
+        <FaMoon
+          className="fill-gray-600 dark:fill-gray-400 group-aria-checked:fill-black dark:group-aria-checked:fill-white align-middle"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+        />
+      </button>
     </div>
   );
 }
