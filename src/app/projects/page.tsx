b99f6d8e1cbc6bd321ffcd18074883b1d21914c7
powerlego <11838540+powerlego@users.noreply.github.com>
2023-06-24 08:51:33 -0400

    added project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-4xl font-bold">Projects</h1>
+      <p className="text-xl">Coming soon...</p>
+    </div>
+  );
+}
